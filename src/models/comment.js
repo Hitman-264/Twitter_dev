e2a205377073c218b797ec8976617755ad244749
Hitman <hitman264212118@gmail.com>
2024-01-19 22:31:39 +0530

    Updated comment model with refPath

--- src/models/comment.js
+++ src/models/comment.js
@@ -5,7 +5,23 @@ const commentSchema = new mongoose.Schema({
     content : {
         type : String,
         required : true,
-    }
+    },
+    userId: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+    },
+    onModel:{
+        type:String,
+        required:true,
+        enum:['Tweet', 'Comment'],
+    },
+    commentable:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        refPath: 'onModel',
+    },
+    
 }, {timestamps:true});
 
 const Comment = mongoose.model('Comment', commentSchema);
