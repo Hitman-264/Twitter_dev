2a05b775c69e2bad793b24be0241890e55bcbf94
Hitman <hitman264212118@gmail.com>
2024-01-21 17:58:39 +0530

    Added Passport logic

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {connect} from './config/database.js';
-
+import passport from 'passport';
 import apiRoutes from './routes/index.js';
-
+import {passportAuth} from './config/jwt_middleware.js'
 const app = express();
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(passport.initialize());
+
+passportAuth(passport);
 app.use('/api', apiRoutes);
 
 
-app.listen(4000, async ()=>{
-    console.log("Server started");
+app.listen(5000, async ()=>{
+    console.log("Server started at PORT 5000");
     await connect();
     console.log("Mongodb connected");
    
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,9 +3,11 @@ import {createTweet, getTweet} from '../../controllers/tweet_controller.js';
 import {toggleLike} from '../../controllers/like_controller.js';
 import { createComment } from '../../controllers/comment_controller.js';
 import { signup, login} from '../../controllers/auth_controller.js';
+
+import {authenticate} from '../../middlewares/authenticate.js';
 const router = express.Router();
 
-router.post('/tweets', createTweet);
+router.post('/tweets', authenticate, createTweet);
 
 router.post('/likes/toggle', toggleLike);
 router.post('/comments', createComment);
